c5f09a2a55ef021160871a51b4f453c85d95a5e6
MyEternity (Home) <spddarkforce47@gmail.com>
2022-01-21 12:01:57 +0300

    L2 task 2

--- Lesson 02/task_2/main.py
+++ Lesson 02/task_2/main.py
@@ -52,3 +52,25 @@
 и достучаться до списка, который и нужно пополнять
 а потом сохранять все в файл
 """
+import datetime
+import json
+
+
+def write_order_to_json(item='name', quantity=0, price=0.1, buyer='person', date='01-01-2001'):
+    with open('orders.json', 'r', encoding='utf-8') as fr:
+        data = json.load(fr)
+        data['orders'].append(
+            {
+                'item': item,
+                'quantity': quantity,
+                'price': price,
+                'buyer': buyer,
+                'date': date
+            })
+    with open('orders.json', 'w', encoding='utf-8') as fw:
+        json.dump(data, fw, indent=4, ensure_ascii=False)
+
+
+write_order_to_json('Телефон', 1, 6700, 'Путин В.В.', '24.09.2017')
+write_order_to_json('Газпром', 1, 10, 'Мишустин М.В.', '11.01.2018')
+write_order_to_json('Россия', 1, 100, 'Ротшильд И.И.', '2.05.2019')
